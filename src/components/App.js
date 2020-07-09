15d4eec5e9ba42f7cd567f8485462f4fd113afa9
Matthew Lean <matt@mattlean.com>
2020-07-08 17:53:53 -0700

    Progress on prototype - Comment code - Add support for trimming HTML paragraphs - Add HTML paragraph operations - Fix typo with txt types where “HTML Entities” should just be “HTML”

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,9 @@ function App() {
   const [origTxtType, setOrigTxtType] = useState('plain')
   const [conversionType, setConversionType] = useState('plain')
   const [preventWidows, setPreventWidows] = useState(false)
-  const [newLineTransform, setNewLineTransform] = useState('')
+  const [newlineTransform, setNewlineTransform] = useState('')
   const [spaceTransform, setSpaceTransform] = useState('')
+  const [htmlTransform, setHTMLTransform] = useState('')
   const [trim, setTrim] = useState(false)
   const [spellCheck, setSpellcheck] = useState(true)
   const [origTxt, setOrigTxt] = useState('')
@@ -15,16 +16,42 @@ function App() {
     let newTxt = txt
 
     if(trim) {
-      const paragraphs = newTxt.split('\n')
-      for(const i in paragraphs) {
-        paragraphs[i] = paragraphs[i].trim()
+      // Trim whitespaces around paragraphs
+      let paragraphs
+
+      if(origTxtType === 'html') {
+        paragraphs = newTxt.split('</p>')
+      } else {
+        paragraphs = newTxt.split('\n')
       }
-      newTxt = paragraphs.join('\n')
+
+      for(const i in paragraphs) {
+        const p = paragraphs[i]
+        if(p) {
+          paragraphs[i] = paragraphs[i].trim()
+        }
+      }
+
+      if(origTxtType === 'html') {
+        newTxt = paragraphs.join('</p>')
+        paragraphs = newTxt.split('<p>')
+
+        for(const i in paragraphs) {
+          const p = paragraphs[i]
+          if(p) {
+            paragraphs[i] = paragraphs[i].trim()
+          }
+        }
+        newTxt = paragraphs.join('<p>')
+      } else {
+        newTxt = paragraphs.join('\n')
+      }
     }
 
     if(conversionType === 'html') {
       if(preventWidows) {
-        const paragraphs = newTxt.split('\n')
+        // Prevent widows
+        const paragraphs = newTxt.split(/\n+/g)
         const words = []
   
         for(const i in paragraphs) {
@@ -56,29 +83,79 @@ function App() {
       }
     }
 
-    if(newLineTransform === 'reduce') {
+    // Newlines
+    if(newlineTransform === 'reduce') {
+      // Reduce multiple newlines
       newTxt = newTxt.replace(/\n+/g, '\n')
-    } else if(newLineTransform === 'remove') {
+    } else if(newlineTransform === 'remove') {
+      // Remove all newlines
       newTxt = newTxt.replace(/\n+/g, '')
     }
 
+    // Spaces
     if(spaceTransform === 'reduce') {
+      // Reduce multiple spaces
       newTxt = newTxt.replace(/ +/g, ' ')
     } else if(spaceTransform === 'remove') {
+      // Remove all spaces
       newTxt = newTxt.replace(/ +/g, '')
     }
+
+    // HTML
+    if(htmlTransform === 'single') {
+      // Wrap paragraphs separated by single newlines with p tags
+      const paragraphs = newTxt.split('\n')
+      for(const i in paragraphs) {
+        const p = paragraphs[i]
+        if(p) {
+          paragraphs[i] = `<p>${paragraphs[i]}</p>`
+        }
+      }
+      newTxt = paragraphs.join('\n')
+    } else if(htmlTransform === 'multiple') {
+      // Wrap paragraphs separated by multiple newlines with p tags
+      const paragraphs = newTxt.split(/\n{2,}/)
+      for(const i in paragraphs) {
+        const p = paragraphs[i]
+        if(p) {
+          paragraphs[i] = `<p>${paragraphs[i]}</p>`
+        }
+      }
+      newTxt = paragraphs.join('\n')
+    } else if(htmlTransform === 'remove') {
+      // Remove all p tags
+      newTxt = newTxt.replace(/(<p[^>]+?>|<p>|<\/p>)/img, '');
+    }
   
+    console.log(JSON.stringify(newTxt))
     return newTxt
   }
 
   let endings
-  
   if(conversionType === 'html') {
     endings = (
       <section>
         <h2>Endings</h2>
         <label>
           <input type="checkbox" checked={preventWidows} onChange={e => setPreventWidows(e.target.checked)}  /> Prevent Widows
+        </label>
+      </section>
+    )
+  }
+
+  let html
+  if(origTxtType === 'html' || conversionType === 'html') {
+    html = (
+      <section>
+        <h2>HTML</h2>
+        <label>
+          Paragraphs
+          <select onChange={e => setHTMLTransform(e.target.value)}>
+            <option value="">None</option>
+            {conversionType === 'html' && <option value="single">Wrap paragraphs separated by single newlines with p tags</option>}
+            {conversionType === 'html' && <option value="multiple">Wrap paragraphs separated by multiple newlines with p tags</option>}
+            <option value="remove">Remove all p tags</option>
+          </select>
         </label>
       </section>
     )
@@ -92,25 +169,25 @@ function App() {
           <h2>Original Text Type</h2>
           <select onChange={e => setOrigTxtType(e.target.value)}>
             <option value="plain">Plain</option>
-            <option value="html">HTML Entities</option>
+            <option value="html">HTML</option>
           </select>
         </section>
         <section>
           <h2>Conversion Type</h2>
           <select onChange={e => setConversionType(e.target.value)}>
             <option value="plain">Plain</option>
-            <option value="html">HTML Entities</option>
+            <option value="html">HTML</option>
           </select>
         </section>
         {conversionType === 'html' ? endings : null}
         <section>
           <h2>Spacing</h2>
           <label>
-            New Lines
-            <select onChange={e => setNewLineTransform(e.target.value)}>
+            Newlines
+            <select onChange={e => setNewlineTransform(e.target.value)}>
               <option value="">None</option>
-              <option value="reduce">Reduce multiple new lines</option>
-              <option value="remove">Remove all new lines</option>
+              <option value="reduce">Reduce multiple newlines</option>
+              <option value="remove">Remove all newlines</option>
             </select>
           </label>
           <label>
@@ -120,11 +197,12 @@ function App() {
               <option value="reduce">Reduce multiple spaces</option>
               <option value="remove">Remove all spaces</option>
             </select>
-            <label>
-              <input type="checkbox" checked={trim} onChange={(e) => setTrim(e.target.checked)} /> Trim whitespaces on paragraphs
-            </label>
+          </label>
+          <label>
+            <input type="checkbox" checked={trim} onChange={(e) => setTrim(e.target.checked)} /> Trim whitespaces around paragraphs
           </label>
         </section>
+        {html}
         <section>
           <h2>Miscellaneous</h2>
           <label>
