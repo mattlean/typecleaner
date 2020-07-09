86ba9edc8c8f0365cf263119a59798c5c52d789d
Matthew Lean <matt@mattlean.com>
2020-07-08 23:50:12 -0700

    Add new cleaning functionality - Add support for replacing newlines in p tags with spaces - Add support for widow prevention when  origTxtType is HTML

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,8 @@ function App() {
   const [preventWidows, setPreventWidows] = useState(false)
   const [newlineTransform, setNewlineTransform] = useState('')
   const [spaceTransform, setSpaceTransform] = useState('')
-  const [htmlTransform, setHTMLTransform] = useState('')
+  const [swapPTagNewlinesSpaces, setSwapPTagNewlinesSpaces] = useState(false)
+  const [pTagTransform, setPTagTransform] = useState('')
   const [trim, setTrim] = useState(false)
   const [spellCheck, setSpellcheck] = useState(true)
   const [origTxt, setOrigTxt] = useState('')
@@ -15,6 +16,19 @@ function App() {
 
   const clean = () => {
     let newTxt = origTxt
+
+    // Replace newlines in p tags with spaces
+    // when original text type is HTML and conversion type is plain
+    if(swapPTagNewlinesSpaces && origTxtType === 'html' && conversionType === 'plain') {
+      const paragraphs = newTxt.split('</p>')
+      for(const i in paragraphs) {
+        const p = paragraphs[i]
+        if(p) {
+          paragraphs[i] = paragraphs[i].replace(/\n+/g, ' ')
+        }
+        newTxt = paragraphs.join('</p>')
+      }
+    }
 
     if(trim) {
       // Trim whitespaces around paragraphs
@@ -52,7 +66,13 @@ function App() {
     if(conversionType === 'html') {
       if(preventWidows) {
         // Prevent widows
-        const paragraphs = newTxt.split(/\n+/g)
+        let paragraphs
+        if(origTxtType === 'html') {
+          paragraphs = newTxt.split(/<\/p>+/g)
+        } else {
+          paragraphs = newTxt.split(/\n+/g)
+        }
+
         const words = []
   
         for(const i in paragraphs) {
@@ -103,7 +123,7 @@ function App() {
     }
 
     // HTML
-    if(htmlTransform === 'single') {
+    if(pTagTransform === 'single') {
       // Wrap paragraphs separated by single newlines with p tags
       const paragraphs = newTxt.split('\n')
       for(const i in paragraphs) {
@@ -113,7 +133,7 @@ function App() {
         }
       }
       newTxt = paragraphs.join('\n')
-    } else if(htmlTransform === 'multiple') {
+    } else if(pTagTransform === 'multiple') {
       // Wrap paragraphs separated by multiple newlines with p tags
       const paragraphs = newTxt.split(/\n{2,}/)
       for(const i in paragraphs) {
@@ -123,9 +143,22 @@ function App() {
         }
       }
       newTxt = paragraphs.join('\n')
-    } else if(htmlTransform === 'remove') {
+    } else if(pTagTransform === 'remove') {
       // Remove all p tags
       newTxt = newTxt.replace(/(<p[^>]+?>|<p>|<\/p>)/img, '');
+    }
+
+    // Replace newlines in p tags with spaces
+    // when original text type and conversion type is HTML
+    if(swapPTagNewlinesSpaces && conversionType === 'html') {
+      const paragraphs = newTxt.split('</p>')
+      for(const i in paragraphs) {
+        const p = paragraphs[i]
+        if(p) {
+          paragraphs[i] = paragraphs[i].replace(/\n+/g, ' ')
+        }
+        newTxt = paragraphs.join('</p>')
+      }
     }
   
     console.log(JSON.stringify(newTxt))
@@ -134,7 +167,14 @@ function App() {
 
   useEffect(() => {
     if(autoClean) clean()
-  }, [origTxtType, conversionType, preventWidows, newlineTransform, spaceTransform, htmlTransform, trim, spellCheck, origTxt, cleanTxt, autoClean])
+  }, [origTxtType, conversionType, preventWidows, newlineTransform, spaceTransform, swapPTagNewlinesSpaces, pTagTransform, trim, spellCheck, origTxt, cleanTxt, autoClean])
+
+  const swapPTagNewlinesSpacesOption = origTxtType === 'html' || conversionType === 'html' ? (
+    <label>
+      <input type="checkbox" checked={swapPTagNewlinesSpaces} onChange={(e) => setSwapPTagNewlinesSpaces(e.target.checked)} />
+      Replace newlines in p tags with spaces
+    </label>
+  ) : null
 
   let endings
   if(conversionType === 'html') {
@@ -156,7 +196,7 @@ function App() {
         <h2>HTML</h2>
         <label>
           Paragraphs
-          <select onChange={e => setHTMLTransform(e.target.value)}>
+          <select onChange={e => setPTagTransform(e.target.value)}>
             <option value="">None</option>
             {conversionType === 'html' && <option value="single">Wrap paragraphs separated by single newlines with p tags</option>}
             {conversionType === 'html' && <option value="multiple">Wrap paragraphs separated by multiple newlines with p tags</option>}
@@ -205,20 +245,24 @@ function App() {
             </select>
           </label>
           <label>
-            <input type="checkbox" checked={trim} onChange={(e) => setTrim(e.target.checked)} /> Trim whitespaces around paragraphs
+            <input type="checkbox" checked={trim} onChange={(e) => setTrim(e.target.checked)} />
+            Trim whitespaces around paragraphs
           </label>
+          {swapPTagNewlinesSpacesOption}
         </section>
         {html}
         <section>
           <h2>Miscellaneous</h2>
           <label>
-            <input type="checkbox" checked={spellCheck} onChange={(e) => setSpellcheck(e.target.checked)} /> Spell Check
+            <input type="checkbox" checked={spellCheck} onChange={(e) => setSpellcheck(e.target.checked)} />
+            Spell Check
           </label>
         </section>
         <section>
           <h2>Clean</h2>
           <label>
-            <input type="checkbox" checked={autoClean} onChange={(e) => setAutoClean(e.target.checked)} /> Auto-Clean
+            <input type="checkbox" checked={autoClean} onChange={(e) => setAutoClean(e.target.checked)} />
+            Auto-Clean
           </label>
           {!autoClean && <button onClick={() => clean()}>Clean</button>}
         </section>
